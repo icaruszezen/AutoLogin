c9b0d7c9d4982d343c09653ef7cc6edc5c2498f6
icaruszezen <38305321+icaruszezen@users.noreply.github.com>
2020-02-15 22:05:20 +0800

    v1.0.0

--- agt.py
+++ agt.py
@@ -1,3 +1,4 @@
+#by zezen lovelive.net.cn
 from selenium import webdriver
 
 import os,time
@@ -9,8 +10,8 @@ driver.refresh() #刷新页面
 driver.maximize_window() #浏览器最大化
 
 
-driver.find_element_by_id("StudentId").send_keys("201831003052")  #替换为自己的学号
-driver.find_element_by_id("Name").send_keys("210012")       #替换为自己的密码
+driver.find_element_by_id("StudentId").send_keys("test")  #替换为自己的学号
+driver.find_element_by_id("Name").send_keys("test")       #替换为自己的密码
 code = driver.find_element_by_id("code-box").text
 driver.find_element_by_id("codeInput").send_keys(code)
 
@@ -19,7 +20,7 @@ driver.find_element_by_id("Submit").click()
 
 time.sleep(0.1) #等待网页加载
 
-#driver.find_element_by_class_name("layui-layer-btn0").click() #进入基本信息采集平台
+#driver.find_element_by_class_name("layui-layer-btn0").click() #进入基本信息采集平台(自动提交需要自己完善代码)
 driver.find_element_by_class_name("layui-layer-btn1").click() #进入温度采集平台
 
 time.sleep(0.1)
