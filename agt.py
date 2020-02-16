dfac62f82b3ee5becdf54eb591953f314693ef42
icaruszezen <38305321+icaruszezen@users.noreply.github.com>
2020-02-16 19:48:22 +0800

    v1.2.0 提交个人温度模块

--- agt.py
+++ agt.py
@@ -1,11 +1,10 @@
-#by zezen lovelive.net.cn
 from selenium import webdriver
-
+import random
 import os,time
 chromedriver = ".\chromedriver.exe" 
 os.environ["webdriver.Chrome.driver"] = chromedriver 
 driver = webdriver.Chrome(chromedriver)
-driver.get("http://bsdt.swun.edu.cn/SPCP/Sys/Web/UserLogin.aspx") 
+driver.get("http://bsdt.swun.edu.cn/SPCP/Sys/Web/UserLogin.aspx") #该处为具体网址
 driver.refresh() #刷新页面
 driver.maximize_window() #浏览器最大化
 
@@ -20,12 +19,23 @@ driver.find_element_by_id("Submit").click()
 
 time.sleep(0.1) #等待网页加载
 
-#driver.find_element_by_class_name("layui-layer-btn0").click() #进入基本信息采集平台
 driver.find_element_by_class_name("layui-layer-btn1").click() #进入温度采集平台
 
 time.sleep(0.1)
 
-driver.find_element_by_id("Temper").send_keys("36.5")  #输入温度
+driver.find_element_by_id("Temper").send_keys("36.",random.randint(5,9))  #输入温度
 
-driver.find_element_by_class_name("save_form").click()
 
+time.sleep(0.1)
+
+localtime = time.localtime(time.time())
+ 
+if localtime[3] <= 23 and localtime[3] >= 16 :
+    if driver.find_element_by_class_name("layui-layer-title").text == "信息":
+        driver.get_screenshot_as_file('./sc.jpg')
+    else:
+        driver.find_element_by_class_name("save_form").click() #提交
+        time.sleep(0.1)
+        driver.get_screenshot_as_file('./sc.jpg')
+else:
+    driver.find_element_by_class_name("save_form").click() #提交
