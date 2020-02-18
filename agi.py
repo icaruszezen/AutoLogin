68751b3e34b71fe12b2dbac14c0bf0e4439500d5
icaruszezen <38305321+icaruszezen@users.noreply.github.com>
2020-02-18 10:52:13 +0800

    Add files via upload

--- agi.py
+++ agi.py
@@ -1,11 +1,10 @@
-#by zezen lovelive.net.cn
 from selenium import webdriver
 
 import os,time
 chromedriver = ".\chromedriver.exe" 
 os.environ["webdriver.Chrome.driver"] = chromedriver 
 driver = webdriver.Chrome(chromedriver)
-driver.get("http://bsdt.swun.edu.cn/SPCP/Sys/Web/UserLogin.aspx") #该处为具体网址
+driver.get("http://bsdt.swun.edu.cn/SPCP/Web/") 
 driver.refresh() #刷新页面
 driver.maximize_window() #浏览器最大化
 
@@ -18,10 +17,10 @@ driver.find_element_by_id("codeInput").send_keys(code)
 
 driver.find_element_by_id("Submit").click()
 
-time.sleep(0.1) #等待网页加载
+time.sleep(0.2) #等待网页加载
 
-driver.find_element_by_class_name("layui-layer-btn0").click() #进入基本信息采集平台
+driver.find_element_by_id("platfrom1").click() #进入基本信息采集平台
 
-time.sleep(0.1)
+time.sleep(0.2)
 
-driver.find_element_by_class_name("save_form").click()
+driver.find_element_by_class_name("save_form").click() #提交
