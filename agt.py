a8adf7409d56b63c3fc0c1482898a4b9e84fa97d
icaruszezen <38305321+icaruszezen@users.noreply.github.com>
2020-02-18 10:50:46 +0800

    v1.3.0 个人温度模块

--- agt.py
+++ agt.py
@@ -1,11 +1,11 @@
-#by zezen lovelive.net.cn
-from selenium import webdriver
+﻿from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import random
 import os,time
 chromedriver = ".\chromedriver.exe" 
 os.environ["webdriver.Chrome.driver"] = chromedriver 
 driver = webdriver.Chrome(chromedriver)
-driver.get("http://bsdt.swun.edu.cn/SPCP/Sys/Web/UserLogin.aspx") #该处为具体网址
+driver.get("http://bsdt.swun.edu.cn/SPCP/Web/")
 driver.refresh() #刷新页面
 driver.maximize_window() #浏览器最大化
 
@@ -18,25 +18,17 @@ driver.find_element_by_id("codeInput").send_keys(code)
 
 driver.find_element_by_id("Submit").click()
 
-time.sleep(0.1) #等待网页加载
+time.sleep(0.2) #等待网页加载
 
-driver.find_element_by_class_name("layui-layer-btn1").click() #进入温度采集平台
+driver.find_element_by_id("platfrom0").click() #进入温度采集平台
 
-time.sleep(0.1)
+time.sleep(0.2)
 
-driver.find_element_by_id("Temper").send_keys("36.",random.randint(5,9))  #输入温度
+Select(driver.find_element_by_id('Temper1')).select_by_value("36")
 
+Select(driver.find_element_by_id('Temper2')).select_by_value(str(random.randint(5,9)))  #温度在36.5-36.9随机生成
 
-time.sleep(0.1)
+time.sleep(0.15)
 
-localtime = time.localtime(time.time())
- 
-if localtime[3] <= 23 and localtime[3] >= 16 :
-    if driver.find_element_by_class_name("layui-layer-title").text == "信息":
-        driver.get_screenshot_as_file('./sc.jpg')
-    else:
-        driver.find_element_by_class_name("save_form").click() #提交
-        time.sleep(0.1)
-        driver.get_screenshot_as_file('./sc.jpg')
-else:
-    driver.find_element_by_class_name("save_form").click() #提交
+driver.find_element_by_class_name("save_form").click() #提交
+driver.get_screenshot_as_file('./sc.jpg')
